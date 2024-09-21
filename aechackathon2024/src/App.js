7ad8744f0f82eda6ac0b971319a62b5a9e2a64c1
brandoncyu <Brandoncyu@gmail.com>
2024-09-21 16:28:41 -0700

    added pins

--- aechackathon2024/src/App.js
+++ aechackathon2024/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import {APIProvider, Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
+import {APIProvider, Map, MapCameraChangedEvent, AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
 import  ZipToGeo  from './utilities/ziptogeo'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import {
   Button,
   Form,
@@ -14,19 +14,36 @@ import {
 } from 'reactstrap'
 
 function App() {
-  const [price, setPrice] = useState('')
-  const [zip, setZipCode] = useState('')
-  const [propertyType, setPropertyType] = useState('')
-  const [latitude, setLatitude] = useState(34.052235)
-  const [longitude, setLongitude] = useState(-118.243683)
+  const [ price, setPrice ] = useState('')
+  const [ zip, setZipCode ] = useState('90017')
+  const [ propertyType, setPropertyType ] = useState('')
+  const [ latitude, setLatitude ] = useState(34.052913)
+  const [ longitude, setLongitude ] = useState(-118.264340)
+
+  const [  posits, setPosits  ] = useState([
+    {key: '1', location: { lat: 34.039378, lng: -118.266300  }},
+    {key: '2', location: { lat: 34.028331, lng: -118.354338  }},
+    {key: '3', location: { lat: 34.028887, lng: -118.317183  }},
+    {key: '4', location: { lat: 34.049841, lng: -118.338460  }}
+  ])
   
   const submit = (e) =>{
     e.preventDefault();
 
-    let lat, long;
-    ({lat, long} =  ZipToGeo(zip));
+    let lat, lng;
+    ({lat, lng} =  ZipToGeo(zip));
     setLatitude(lat)
-    setLongitude(long)
+    setLongitude(lng)
+
+    let zipNumber = parseInt(zip);
+
+    setPosits([
+    {key: '1', location: ZipToGeo((zipNumber - 2).toString()) },
+    {key: '2', location: ZipToGeo((zipNumber - 1).toString()) },
+    {key: '3', location: ZipToGeo((zipNumber + 1).toString()) },
+    {key: '4', location: ZipToGeo((zipNumber + 2).toString()) }
+    ])
+
   }
 
 
@@ -61,12 +78,23 @@ function App() {
       <div id="googlemaps">
       <APIProvider apiKey={'AIzaSyBR4OUYOMC4iSFYayAdkfgjfc_itpVDGfA'} onLoad={() => console.log('Maps API has loaded.')}>
         <Map
-            defaultZoom={13}
+            defaultZoom={10}
             center={ { lat: latitude, lng: longitude } }
+    mapId='DEMO_MAP_ID'
             onCameraChanged={ (ev: MapCameraChangedEvent) =>
             console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
             }>
         </Map>
+          <AdvancedMarker
+            position={{ lat: latitude, lng: longitude }}>
+          </AdvancedMarker>
+          {posits.map( (posit) => (
+            <AdvancedMarker
+              key={ posit.key }
+              position={ posit.location }>
+              <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
+            </AdvancedMarker>
+          ))}
         </APIProvider>
       </div>
   </div>
