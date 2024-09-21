f87ca13f5a1d41760995b8a3c6894ec47569a5c3
brandoncyu <Brandoncyu@gmail.com>
2024-09-21 14:29:20 -0700

    added map

--- aechackathon2024/src/App.js
+++ aechackathon2024/src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import {APIProvider, Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
+import { useState } from 'react';
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Container,
+  Row,
+  Col,
+  Input
+} from 'reactstrap'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [price, setPrice] = useState('')
+  const [zip, setZipCode] = useState('')
+  const [propertyType, setPropertyType] = useState('')
+  
+  const submit = (e) =>{
+    e.preventDefault();
+
+    console.log(price, zip, propertyType)
+  }
+
+  return (<div className="App">
+      <h1 id ="title">Find Your Apartment</h1>
+      <Container >
+        <Row>
+          <Col></Col>
+          <Col xl={3}>
+            <Form className="p-4" id="initial-form" onSubmit={submit} >
+              <FormGroup>
+                <Label htmlFor="price">Price</Label>
+                <Input type="number" name="price" id="price" placeholder="Enter your price" value={price} onChange={(e) => setPrice(e.target.value)} />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="zip">Zip Code</Label>
+                <Input type="number" name="zip" id="zip" placeholder="What is your zip code?" value={zip} onChange={(e) => setZipCode(e.target.value)}  />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="propertyType">Property Type</Label><br />
+                <Input type="select" name="propertyType" id="propertyType" value={propertyType} onChange={(e) => setPropertyType(e.target.value)}  >
+                  <option value="2">Apartment</option>
+                  <option value="3">House</option>
+                </Input>
+              </FormGroup>
+              <Button color="primary">Find Your Apartment!</Button>
+            </Form>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+      <div id="googlemaps">
+      <APIProvider apiKey={'AIzaSyBR4OUYOMC4iSFYayAdkfgjfc_itpVDGfA'} onLoad={() => console.log('Maps API has loaded.')}>
+        <Map
+            defaultZoom={13}
+            defaultCenter={ { lat: -33.860664, lng: 151.208138 } }
+            onCameraChanged={ (ev: MapCameraChangedEvent) =>
+            console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
+            }>
+        </Map>
+        </APIProvider>
+      </div>
+  </div>
   );
 }
 
